90adfe2efaba6a82423488ce3b1a4dd731168c25
mfaizudd <mfaizudd@gmail.com>
2020-12-23 17:12:26 +0700

    feat: add seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Mega Admind',
+            'phone' => '089643414876',
+            'email' => 'admind@admin.com',
+            'password' => Hash::make('password'),
+            'role' => 'Admin'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Filthy Customer',
+            'phone' => '085225751050',
+            'email' => 'customer@filthy.com',
+            'password' => Hash::make('password'),
+            'role' => 'Customer'
+        ]);
+        User::factory()
+                ->times(50)
+                ->create();
+    }
+}
