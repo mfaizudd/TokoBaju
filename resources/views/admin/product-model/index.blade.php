cd9c70a4d6677ec57fd8e9fb121dcaa8bbff8b36
mfaizudd <mfaizudd@gmail.com>
2020-12-28 23:55:59 +0700

    fix: update product model index

--- resources/views/admin/product-model/index.blade.php
+++ resources/views/admin/product-model/index.blade.php
@@ -1,35 +1,36 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Product') }}
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('ProductModel') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <table class="w-full mb-5 table table-auto">
+                    <table class="table w-full mb-5 table-auto">
                         <thead>
                             <tr>
-                                <th>Name</th>
-                                <th>Brand</th>
+                                <th>Size</th>
+                                <th>Color</th>
+                                <th>Price</th>
                                 <th class="w-44">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($products as $product)
-                                <tr class="hover:bg-gray-200 transition-colors">
-                                    <td class="border-b-2 p-2">
-                                        <a href="{{ route('admin.product.show', $product->id) }}">{{ $product->name }}</a>
+                            @foreach ($productmodels as $productmodel)
+                                <tr class="transition-colors hover:bg-gray-200">
+                                    <td class="p-2 border-b-2">
+                                        <a href="{{ route('admin.productmodel.show', $productmodel->id) }}">{{ $productmodel->name }}</a>
                                     </td>
-                                    <td class="border-b-2 p-2">{{ $product->brand }}</td>
-                                    <td class="p-2 flex">
-                                        <x-button-link href="{{ route('admin.product.edit', $product->id) }}">
+                                    <td class="p-2 border-b-2">{{ $productmodel->brand }}</td>
+                                    <td class="flex p-2">
+                                        <x-button-link href="{{ route('admin.productmodel.edit', $productmodel->id) }}">
                                             Edit
                                         </x-button-link>
-                                        <form class="mx-2" action="{{ route('admin.product.destroy', $product->id) }}" method="post">
+                                        <form class="mx-2" action="{{ route('admin.productmodel.destroy', $productmodel->id) }}" method="post">
                                             @csrf
                                             @method('DELETE')
                                             <x-button class="bg-red-800 hover:bg-red-500">
@@ -43,7 +44,7 @@
                     </table>
 
                     <div class="flex justify-end">
-                        <x-button-link href="{{ route('admin.product.create') }}">
+                        <x-button-link href="{{ route('admin.productmodel.create') }}">
                             Create
                         </x-button-link>
                     </div>
